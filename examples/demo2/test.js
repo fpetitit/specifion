1b0a41c4f2ef1bd3271cfcdd1f976c6c2accc8b7
François Petitit <francois.petitit@gmail.com>
2024-04-10 12:46:22 +0200

    add demo 2 - WIP

--- examples/demo2/test.js
+++ examples/demo2/test.js
@@ -1,5 +1,22 @@
 import * as cheerio from 'cheerio';
 import fs from 'node:fs';
 
+const add = (a, b) => a + b;
+
 const html = fs.readFileSync('examples/demo2/test.html', 'utf8');
 console.log(html);
+
+const $ = cheerio.load(html);
+
+const testCall = $('[data-specifionCall]').text();
+console.log('function called : ', testCall);
+const testArgs = $('[data-specifionSet]').text();
+for(let i = 0; i < testArgs.length; i++){
+  console.log(`parameter ${i} : `, testArgs[i]);
+}
+
+const textExpected = $('[data-textExpected]').text();
+console.log('expected : ', textExpected);
+
+const result = (new Function(['arg1'], testCall))(testArgs);
+console.log('result : ', result);
